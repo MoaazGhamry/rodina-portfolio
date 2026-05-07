ab9b07aca779c156db572448d7f1f9fe584a01f8
Moaaz Elghamry <29212023100207@meceng.znu.edu.eg>
2026-05-07 21:54:54 +0300

    Admin: Cloudinary Image Fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
       { protocol: 'https', hostname: 'rodina-portfolio-admin-f0b1c.firebasestorage.app' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
 };
